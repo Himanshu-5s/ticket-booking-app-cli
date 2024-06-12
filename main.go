4de660dd2a55d6faca48ad291c532427ee1bd4d9
Himanshu <himanshusharma5205@gmail.com>
2024-06-12 23:12:34 +0530

    user input validation

--- main.go
+++ main.go
@@ -28,8 +28,6 @@ func main() {
 		var phoneNumber string
 		var userTickets int
 
-		//userName = "John Doe"
-		//userTickets = 5
 		fmt.Println("Please enter your first name: ")
 		fmt.Scan(&firstName)
 		fmt.Println("Please enter your last name: ")
@@ -41,22 +39,39 @@ func main() {
 		fmt.Println("Please enter the number of tickets you want to book: ")
 		fmt.Scan(&userTickets)
 
-		// logic for updating the remaining tickets
-		remainingTicket = remainingTicket - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
+		// user input validation
+		isValidName := len(firstName) > 2 && len(lastName) > 2
+		isValidEmail := strings.Contains(email, "@") && strings.Contains(email, ".")
+		isValidPhoneNumber := len(phoneNumber) == 11
+		isValidUserTcikets := userTickets > 0 && userTickets <= remainingTicket
 
-		fmt.Printf("Thank you %v %v for booking %v tickets for the %v event\n", firstName, lastName, userTickets, eventName)
-		fmt.Printf("Your ticket(s) will be sent to your email: %v and phone number: %v\n", email, phoneNumber)
-		fmt.Printf("There are %v tickets remaining for the %v event\n", remainingTicket, eventName)
-		fmt.Println("We are happy to have you here!")
+		if isValidName && isValidEmail && isValidPhoneNumber && isValidUserTcikets {
+			// logic for updating the remaining tickets
+			remainingTicket = remainingTicket - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+			fmt.Printf("Thank you %v %v for booking %v tickets for the %v event\n", firstName, lastName, userTickets, eventName)
+			fmt.Printf("Your ticket(s) will be sent to your email: %v and phone number: %v\n", email, phoneNumber)
+			fmt.Printf("There are %v tickets remaining for the %v event\n", remainingTicket, eventName)
+			fmt.Println("We are happy to have you here!")
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			//fmt.Printf("The list of all bookings for the %v event are: %v\n", eventName, bookings)
+			fmt.Printf("The list of all bookings for the %v event are: %v\n", eventName, firstNames)
+
+			// check if all tickets have been booked(can it be a else if statement?)
+			if remainingTicket == 0 {
+				fmt.Println("All tickets have been booked for the event. Please come back next year")
+				break
+			}
+
+		} else {
+			fmt.Printf("Sorry, your booking was not successful due to invalid input. Please check your details and try again\n")
 		}
-
-		//fmt.Printf("The list of all bookings for the %v event are: %v\n", eventName, bookings)
-		fmt.Printf("The list of all bookings for the %v event are: %v\n", eventName, firstNames)
 	}
 }
