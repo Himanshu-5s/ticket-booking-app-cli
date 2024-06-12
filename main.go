a3b89abc2aa985064fe307cf0eb3f19f63bb831f
Himanshu <himanshusharma5205@gmail.com>
2024-06-12 16:31:46 +0530

    version 2

--- main.go
+++ main.go
@@ -16,15 +16,36 @@ func main() {
 	fmt.Printf("The total number of tickets for the event is %v and %v are still available\n", eventTicket, remainingTicket)
 	fmt.Println("We are happy to have you here!")
 
-	var userName string
-	// ask user to enter their name
-	var userTickets int
+	var bookings []string
 
-	userName = "John Doe"
-	userTickets = 5
-	fmt.Println("Please enter your name: ")
-	fmt.Scan(&userName)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var phoneNumber string
+		var userTickets int
 
-	fmt.Printf("user %v booked %v tickets.\n", userName, userTickets)
+		//userName = "John Doe"
+		//userTickets = 5
+		fmt.Println("Please enter your first name: ")
+		fmt.Scan(&firstName)
+		fmt.Println("Please enter your last name: ")
+		fmt.Scan(&lastName)
+		fmt.Println("Please enter your email: ")
+		fmt.Scan(&email)
+		fmt.Println("Please enter your phone number: ")
+		fmt.Scan(&phoneNumber)
+		fmt.Println("Please enter the number of tickets you want to book: ")
+		fmt.Scan(&userTickets)
 
+		// logic for updating the remaining tickets
+		remainingTicket = remainingTicket - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets for the %v event\n", firstName, lastName, userTickets, eventName)
+		fmt.Printf("Your ticket(s) will be sent to your email: %v and phone number: %v\n", email, phoneNumber)
+		fmt.Printf("There are %v tickets remaining for the %v event\n", remainingTicket, eventName)
+		fmt.Println("We are happy to have you here!")
+		fmt.Printf("The list of all bookings for the %v event are: %v\n", eventName, bookings)
+	}
 }
